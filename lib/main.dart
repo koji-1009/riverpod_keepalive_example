ba2134000d96cb631a27d2af964b26f105d3160d
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-12-21 09:01:35 +0900

    Add example code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +22,110 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends ConsumerWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example keepAlive'),
+      ),
+      body: Center(
+        child: Column(
+          spacing: 16,
+          children: [
+            FilledButton(
+              onPressed: () async {
+                print('readAndKeepAlive:Button: started');
+                final value = await ref.read(
+                  readAndKeepAliveProvider.future,
+                );
+                print('readAndKeepAlive:Button: finished: $value');
+              },
+              child: const Text('Read readAndKeepAlive'),
+            ),
+            FilledButton.tonal(
+              onPressed: () async {
+                print('readAndFialedKeepAlive:Button: started');
+                final value = await ref.read(
+                  readAndFialedKeepAliveProvider.future,
+                );
+                print('readAndFialedKeepAlive:Button: finished: $value');
+              },
+              child: const Text('Read readAndFialedKeepAlive'),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+const keepDuration = Duration(
+  seconds: 10,
+);
+
+Future<String> computeValue(String value) async {
+  await Future.delayed(
+    const Duration(
+      seconds: 5,
+    ),
+  );
+  return value;
+}
+
+@riverpod
+Future<String> readAndKeepAlive(Ref ref) async {
+  print('readAndKeepAlive:Provider: Started');
+  ref
+    ..onCancel(() {
+      print('readAndKeepAlive:Provider: Canceled');
+    })
+    ..onResume(() {
+      print('readAndKeepAlive:Provider: Resumed');
+    })
+    ..onDispose(() {
+      print('readAndKeepAlive:Provider: Disposed');
+    });
+
+  final link = ref.keepAlive();
+  final String result;
+  try {
+    result = await computeValue('Read and keep alive');
+    print('readAndKeepAlive:Provider: Get result');
+  } on Exception catch (_) {
+    link.close();
+    rethrow;
+  }
+
+  print('readAndKeepAlive:Provider: Finished');
+  return result;
+}
+
+@riverpod
+Future<String> readAndFialedKeepAlive(Ref ref) async {
+  print('readAndFialedKeepAlive:Provider: Started');
+  ref
+    ..onCancel(() {
+      print('readAndFialedKeepAlive:Provider: Canceled');
+    })
+    ..onResume(() {
+      print('readAndFialedKeepAlive:Provider: Resumed');
+    })
+    ..onDispose(() {
+      print('readAndFialedKeepAlive:Provider: Disposed');
+    });
+  final result = await computeValue('Read and failed keep alive');
+  print('readAndFialedKeepAlive:Provider: Get result');
+
+  ref.keepAlive();
+
+  print('readAndFialedKeepAlive:Provider: Finished');
+  return result;
 }
---
+++ lib/main.g.dart
@@ -0,0 +1,46 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$readAndKeepAliveHash() => r'757c0c87d18cbe4bb6e52fc0e6dcc31fdb366aa9';
+
+/// See also [readAndKeepAlive].
+@ProviderFor(readAndKeepAlive)
+final readAndKeepAliveProvider = AutoDisposeFutureProvider<String>.internal(
+  readAndKeepAlive,
+  name: r'readAndKeepAliveProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$readAndKeepAliveHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ReadAndKeepAliveRef = AutoDisposeFutureProviderRef<String>;
+String _$readAndFialedKeepAliveHash() =>
+    r'446100df1256050626090710520ea14f43aefa9f';
+
+/// See also [readAndFialedKeepAlive].
+@ProviderFor(readAndFialedKeepAlive)
+final readAndFialedKeepAliveProvider =
+    AutoDisposeFutureProvider<String>.internal(
+  readAndFialedKeepAlive,
+  name: r'readAndFialedKeepAliveProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$readAndFialedKeepAliveHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ReadAndFialedKeepAliveRef = AutoDisposeFutureProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
